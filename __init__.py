e3fe31e1c7102f5409dff7cdf678e5513e7a1e62
ggjk26 <conater@outlook.com>
2026-06-28 16:14:20 +0800

    Refactor registration and update version info
    Updated version and Blender compatibility, refactored registration process.

--- __init__.py
+++ __init__.py
@@ -1,54 +1,32 @@
 bl_info = {
     "name": "MMD Features Append",
     "author": "Your Name",
-    "version": (3, 1, 0),
-    "blender": (4, 0, 0),
+    "version": (0, 1, 0),
+    "blender": (5, 1, 0),
     "location": "View3D > Sidebar > MMFA",
     "description": "MMD 物理优化、PBR 转换、模型修复、摄像机构图、防穿模、布料转换、三渲二",
     "category": "Animation",
 }
 
-if "bpy" in locals():
-    import importlib
-    importlib.reload(utils)
-    importlib.reload(physics)
-    importlib.reload(materials)
-    importlib.reload(repair)
-    importlib.reload(camera)
-    importlib.reload(cloth)
-    importlib.reload(toon_shading)
-    importlib.reload(ui)
-else:
-    from . import utils
-    from . import physics
-    from . import materials
-    from . import repair
-    from . import camera
-    from . import cloth
-    from . import toon_shading
-    from . import ui
-
 import bpy
 
+from .ui import PANEL_PT_main
+from .ops import *
+
+classes = [
+    PANEL_PT_main,
+    SCENE_OT_fix,
+    MATERIAL_OT_pbr,
+    CAMERA_OT_auto,
+    EYE_OT_system,
+    PHYSICS_OT_cloth,
+    RENDER_OT_preset,
+]
+
 def register():
-    utils.register()
-    physics.register()
-    materials.register()
-    repair.register()
-    camera.register()
-    cloth.register()
-    toon_shading.register()
-    ui.register()
+    for c in classes:
+        bpy.utils.register_class(c)
 
 def unregister():
-    utils.unregister()
-    physics.unregister()
-    materials.unregister()
-    repair.unregister()
-    camera.unregister()
-    cloth.unregister()
-    toon_shading.unregister()
-    ui.unregister()
-
-if __name__ == "__main__":
-    register()
+    for c in reversed(classes):
+        bpy.utils.unregister_class(c)
